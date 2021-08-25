52b5985baeaae6e2147f4cf88f7493140466204f
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-08-25 21:41:44 +0530

    taking references

---
+++ app.js
@@ -0,0 +1,5 @@
+const buyPrice = document.querySelector("#buy-price");
+const quantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const calculate = document.querySelector("#calculate");
+const output = document.querySelector("#output");
